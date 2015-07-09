3f17aefb9d48a53dedfb5f94a6bac6040d4b5619
Guilherme A. Fôlego <gfolego@gmail.com>
2015-07-08 23:30:40 -0300

    Script that process Rmd with knitr

---
+++ process.R
@@ -0,0 +1,35 @@
+#!/usr/bin/Rscript --vanilla
+
+
+# process.R
+# Copyright (C) 2015 Guilherme A. Fôlego (gfolego@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+# This script processes the implementation for
+#   Reproducible Research Course Peer Assessment 1
+# For more information, please refer to
+#   https://github.com/gfolego/RepData_PeerAssessment1
+
+# This script makes an attempt to follow Google's R Style Guide
+# For more information, please refer to
+#   https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml
+
+
+library(knitr)
+knit2html("PA1_template.Rmd")
+browseURL("PA1_template.html")
+
