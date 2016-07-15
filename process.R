1db7e6899de8ab1ff82a72e046630896299c3119
Guilherme A. Fôlego <gfolego@gmail.com>
2016-07-15 13:00:01 -0300

    Update license information

--- process.R
+++ process.R
@@ -2,7 +2,7 @@
 
 
 # process.R
-# Copyright (C) 2015 Guilherme A. Fôlego (gfolego@gmail.com)
+# Copyright (C) 2015 Guilherme Folego (gfolego@gmail.com)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
